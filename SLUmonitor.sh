de7ea180b70dc29622ba88a5ed596c4393e213e3
fulvius31 <alessandrosangiorgi31@gmail.com>
2018-08-10 12:09:56 -0500

    - Added script to quick monitors management at SLU

---
+++ SLUmonitor.sh
@@ -0,0 +1,3 @@
+optirun intel-virtual-output
+xrandr --output eDP1 --mode 2560x1440 --pos 1280x0 --rotate normal --output VIRTUAL1 --mode 1280x1024 --rotate normal --pos 3840x416 --rotate normal --output DP1 --mode 1280x1024 --rotate normal --pos 0x416
+
