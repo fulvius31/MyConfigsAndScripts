18cac88cc460c6cb139b6af42067efb12e61ebd1
Alessandro Sangiorgi <alessandrosangiorgi31@gmail.com>
2018-08-17 12:57:51 -0500

    Scale on laptop monitor

--- SLU/SLUmonitor.sh
+++ SLU/SLUmonitor.sh
@@ -1,3 +1,3 @@
 optirun intel-virtual-output
-xrandr --output eDP1 --mode 2560x1440 --pos 1280x0 --rotate normal --output VIRTUAL1 --mode 1280x1024 --rotate normal --pos 3840x416 --rotate normal --output DP1 --mode 1280x1024 --rotate normal --pos 0x416
+xrandr --output eDP1 --scale 0.85x0.85 --mode 2560x1440 --pos 1280x0 --rotate normal --output VIRTUAL1 --mode 1280x1024 --rotate normal --pos 3840x416 --rotate normal --output DP1 --mode 1280x1024 --rotate normal --pos 0x416
 
